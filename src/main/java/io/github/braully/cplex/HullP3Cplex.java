46a6c037109d17d670ad54b8d04619698983b42d
Braully Silva <braully.silva@ifgoiano.edu.br>
2022-04-16 21:37:41 -0300

    pequenas correções

--- src/main/java/io/github/braully/cplex/HullP3Cplex.java
+++ src/main/java/io/github/braully/cplex/HullP3Cplex.java
@@ -39,18 +39,25 @@ public class HullP3Cplex {
 //#   - Ideia geral:
 //#     - Restrição: - $\forall i\in V(G)$ se $S_{i}=1$ então $v\in S$
 //#                  - $\forall i \in S$  $H_{i}=1$
-//#                  - $H_{v}=1$ se $v\in H$ 
+//#                  - $H_{v}=1$ se $v\in H$
+    static int[][] matrix = new int[][]{
+        {0, 1, 0, 0, 1},
+        {1, 0, 1, 0, 0},
+        {0, 1, 0, 1, 0},
+        {0, 0, 1, 0, 1},
+        {1, 0, 0, 1, 0}
+    };
+
     public static void main(String[] args) {
         // Create the modeler/solver object
         try (IloCplex cplex = new IloCplex()) {
             IloIntVar[] var;
             IloRange[] rng;
-            int n = 3;
+            int n = matrix.length;
             //k é o max iteracoes
             int k = n;
 
-            String[] varname = {"x1", "x2", "x3"};
-            IloIntVar[] x = cplex.boolVarArray(n, varname);
+            IloIntVar[] x = cplex.boolVarArray(n);
             var = x;
 
             //Variavel yi,n
@@ -61,7 +68,7 @@ public class HullP3Cplex {
 
             //yi0=x0
             for (int i = 0; i < n; i++) {
-                y[i][0] = x[i];
+                y[0][i] = x[i];
             }
 
             //    Maximize
@@ -71,11 +78,16 @@ public class HullP3Cplex {
             //    Subject To
             //     - x1 + x2 + x3 <= 20
             //     x1 - 3 x2 + x3 <= 30
-            rng = new IloRange[1];
+//            rng = new IloRange[1];
 //            rng[0] = new IloRange[2];
-
             //X tem que ter pelo menos duas variavies
-            rng[0] = cplex.addGe(cplex.sum(x), 2.0, "c1");
+//            rng[0] = 
+            cplex.addGe(cplex.sum(x), 2, "c1");
+
+            //Ao final de k iterações, todo os vertices precisa estar em H(S)
+            for (int i = 0; i < k - 1; i++) {
+                cplex.addGe(y[k - 1][i], 1);
+            }
 
             //yij+1>=yij
             for (int i = 1; i < n; i++) {
@@ -84,11 +96,20 @@ public class HullP3Cplex {
                 }
             }
 
+            //Se yij tem dois vizinhos em y*j-1, então 
+            //yij>=1
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < k - 1; j++) {
+//                    cplex.addGe(, 1);
+                    //yij>=(N(j)*yij-1)/2
+                    cplex.addGe(y[i][j], cplex.prod(cplex.scalProd(matrix[j], y[i - 1]), 0.5));
+                }
+            }
+
             // solve the model and display the solution if one was found
             if (cplex.solve()) {
                 double[] xv = cplex.getValues(var);
 //                double[] slack = cplex.getSlacks(rng[0]);
-
                 cplex.output().println("Solution status = " + cplex.getStatus());
                 cplex.output().println("|S|=" + cplex.getObjValue());
 
@@ -97,6 +118,25 @@ public class HullP3Cplex {
                 for (int j = 0; j < nvars; ++j) {
                     if (xv[j] >= 0) {
                         cplex.output().print(j + ", ");
+                    }
+                }
+                cplex.output().println("}");
+
+                double[] yk = cplex.getValues(y[k - 1]);
+                double[] y0 = cplex.getValues(y[0]);
+                nvars = yk.length;
+                cplex.output().print("YK={");
+                for (int j = 0; j < nvars; ++j) {
+                    if (yk[j] >= 0) {
+                        cplex.output().print(j + ":" + yk[j] + ", ");
+                    }
+                }
+                cplex.output().println("}");
+
+                cplex.output().print("Y0={");
+                for (int j = 0; j < nvars; ++j) {
+                    if (y0[j] >= 0) {
+                        cplex.output().print(j + ":" + y0[j] + ", ");
                     }
                 }
                 cplex.output().println("}");
