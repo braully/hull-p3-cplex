dd90b4677a8954b9c85ad0d1b5590ed1d70d4dbe
Braully Silva <braully.silva@ifgoiano.edu.br>
2022-05-20 13:42:24 -0300

    adjust size for small and medium screen.

---
+++ src/main/java/tmp/OrederGraphG6file.java
@@ -0,0 +1,67 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 strike.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package tmp;
+
+import io.github.braully.graph.UndirectedSparseGraphTO;
+import io.github.braully.graph.UtilGraph;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author strike
+ */
+public class OrederGraphG6file {
+
+    public static void main(String... args) throws Exception {
+        String dir = "/home/strike/Workspace/hull-cplex/";
+        String unsortedFile = "hog-graphs-ge20-le50.g6";
+        String sortedFile = "hog-graphs-ge20-le50-ordered.g6";
+        BufferedReader inputFile = new BufferedReader(new FileReader(new File(dir + unsortedFile)));
+
+        String linha = null;
+        TreeMap<Integer, List<String>> map = new TreeMap<>();
+        while ((linha = inputFile.readLine()) != null) {
+            UndirectedSparseGraphTO<Integer, Integer> loadGraphG6 = UtilGraph.loadGraphG6(linha);
+            if (loadGraphG6 != null) {
+                int k = loadGraphG6.getVertexCount();
+                map.computeIfAbsent(k, key -> new ArrayList<>()).add(linha);
+            }
+        }
+        FileWriter filew = new FileWriter(new File(dir, sortedFile));
+        for (List<String> strs : map.values()) {
+            for (String s : strs) {
+                filew.write(s);
+                filew.write("\n");
+            }
+
+        }
+        filew.close();
+    }
+}
